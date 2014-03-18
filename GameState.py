aa917bcb313037c68d9f550d67899442f597c15a
Esurnir <esurnir@gmail.com>
2014-03-18 15:46:02 -0400

    typo change

--- GameState.py
+++ GameState.py
@@ -24,7 +24,7 @@ class State():
     def setPickedUpPiece(self,x):
         self.pickedUpPiece = x
 
-    def getPieckedUpPiece(self,x):
+    def getPieckedUpPiece(self):
         return self.pickedUpPiece
 
         
