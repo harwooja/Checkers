7834c57686ec94cb66e3a7eac3bf6f9065e773de
Richard Damra <damrarv@mcmaster.ca>
2014-03-21 19:10:10 -0400

    commented

--- Saving.py
+++ Saving.py
@@ -1,18 +1,18 @@
 import pickle 
 def saveState(state):
-    file_name = "Savefile"
-    fileObject = open(file_name,'wb')
-    pickle.dump(state,fileObject)
-    fileObject.close()
+    file_name = "Savefile"#name of the savefile
+    fileObject = open(file_name,'wb')#creates the savefile
+    pickle.dump(state,fileObject)#saves the gameState into the savefile
+    fileObject.close()#closes savefile
 
 def loadState():
     try:
-        file_name = "Savefile"
-        fileObject = open(file_name,'r')
-        loadedList = pickle.load(fileObject)
-        fileObject.close()
-        return loadedList
-    except ValueError:
+        file_name = "Savefile"#name of file to load
+        fileObject = open(file_name,'r')#opens file in read mode
+        loadedList = pickle.load(fileObject)#loadedList is assigned to whatever was saved
+        fileObject.close()#closes the file
+        return loadedList#returns the gameState
+    except ValueError:#checks to see if there is a savefile to begin with, via try and catch
         print("No save file detected")
 
     
