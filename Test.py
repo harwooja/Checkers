f2a444aac912271ae65b8a06e87703e4f5c888fc
Robert Kason Gorrie <rob-gxiv@hotmail.com>
2014-04-11 17:04:42 -0400

    started the test class. donno what im doing =D

--- Test.py
+++ Test.py
@@ -1,1 +1,16 @@
 import pygame,sys,Board,Constants,InputManager,Menu,GameState
+from pygame.locals import *
+pygame.init()
+size = Constants.SIZE
+window = pygame.display.set_mode(size)
+pygame.display.set_caption("Checkers 2ME3/2AA4 Testing")
+
+def testBoard(board):
+
+
+def testAI(board):
+
+
+def testSaveLoad(board):
+
+
